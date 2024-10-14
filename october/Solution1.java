83493aea24c016bd5b0a8b1c584ffa1063a81b4a
parkmookeun <sohappynow12@naver.com>
2024-10-14 11:24:57 +0900

    first_commit

---
+++ october/Solution1.java
@@ -0,0 +1,8 @@
+package sparta.codekata.october;
+
+public class Solution1 {
+    public static int solution(int num1, int num2) {
+        int answer = num1- num2;
+        return answer;
+    }
+}
