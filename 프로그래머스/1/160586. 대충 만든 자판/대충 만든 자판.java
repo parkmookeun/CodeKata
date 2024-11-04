d5b49a8bfdd67f85dd72c537d0bc4fd387e31d65
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-11-04 10:14:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 2.44 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        //정답을 담을 배열
+        int[] answer = new int[targets.length];
+        
+        //keymap을 전부 합쳐서 만든 효율적인 map
+        Map<Character,Integer> map = new HashMap<>();
+        for(int i=0; i<keymap.length; i++){
+            String key = keymap[i];
+            for(int j=0; j<key.length(); j++){
+                char target = key.charAt(j);
+                if(map.get(target) == null){
+                    map.put(target,j+1);
+                }else{
+                    if(map.get(target) > j+1) map.put(target,j+1);
+                }
+            }
+        }
+        //이제 키판에 맞춰서 targets들 검사
+        for(int i=0; i<targets.length; i++){
+            int subAnswer = 0;
+            for(int j=0; j<targets[i].length(); j++){
+                //만약에 키판에 문자가 없는 경우
+                if(map.get(targets[i].charAt(j)) == null){
+                    subAnswer = -1;
+                    break;
+                }
+                //있는 경우
+                subAnswer += map.get(targets[i].charAt(j));
+            }
+            answer[i] = subAnswer;
+        }
+        
+        return answer;
+    }
+}
