cd39c27dd0d754660d891093c972c11aa7ab34f7
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-11-07 12:13:57 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 15.55 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('R',0);map.put('T',0);
+        map.put('C',0);map.put('F',0);
+        map.put('J',0);map.put('M',0);
+        map.put('A',0);map.put('N',0);
+
+        for(int i=0; i<survey.length; i++){
+            if(choices[i] > 4){
+                map.put(survey[i].charAt(1),map.get(survey[i].charAt(1)) + choices[i]-4);
+            }else if(choices[i] < 4){
+                map.put(survey[i].charAt(0),map.get(survey[i].charAt(0)) + 4- choices[i]);
+            }
+        }
+
+        answer += map.get('R') >= map.get('T') ? 'R' : 'T';
+        answer += map.get('C') >= map.get('F') ? 'C' : 'F';
+        answer += map.get('J') >= map.get('M') ? 'J' : 'M';
+        answer += map.get('A') >= map.get('N') ? 'A' : 'N';
+        
+        return answer;
+    }
+}
