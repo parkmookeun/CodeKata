3dc1e07ac70cdf69942de84796d605bdce86d0bf
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-11-11 10:07:21 +0900

    [level 1] Title: 달리기 경주, Time: 300.86 ms, Memory: 367 MB -BaekjoonHub

---
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        Map<String,Integer> currSituation = new HashMap<>();
+        //현재 상황 초기화
+        for(int i=0; i<players.length; i++){
+            currSituation.put(players[i],i);
+            answer[i] = players[i];
+        }
+        //호출에 따라 순위 변경
+        for(int i=0; i<callings.length; i++){
+            //바꿀 idx 선언
+            int idx = currSituation.get(callings[i]);
+            //바꾸기
+            String temp = answer[idx];
+            answer[idx] = answer[idx-1];
+            answer[idx-1] = temp;
+            //맵에도 적용
+            currSituation.put(answer[idx],idx);
+            currSituation.put(answer[idx-1],idx-1);
+            
+        }
+        return answer;
+    }
+}
