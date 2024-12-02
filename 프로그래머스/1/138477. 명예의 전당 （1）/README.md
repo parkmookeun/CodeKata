e20ee76d61caeb6c771cd739e3648ae87bd49075
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-12-02 09:15:39 +0900

    [level 1] Title: 명예의 전당 (1), Time: 16.13 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 16.70 ms
+메모리: 88.1 MB, 시간: 16.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 25일 14:11:23
+2024년 12월 02일 09:15:36
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -2,12 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int k, int[] score) {
+        //명예의 전당에 값을 넣고
+        //정렬을 시킨다. k번째 인덱스에 있는 값을 정답배열에 넣는다.
+        
+        
+        //정답 배열
         int[] answer = new int[score.length];
+        
         ArrayList<Integer> list = new ArrayList<>();
+        
         for(int i=0; i<score.length; i++){
+            
             list.add(score[i]);
             list.sort(Comparator.reverseOrder());
+            
             answer[i] = i < k ? list.get(i) : list.get(k-1);
+            
         }
 
         return answer;
