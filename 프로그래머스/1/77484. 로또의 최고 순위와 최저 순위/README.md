c013a1e1161debaffb64a4c09ff12b2b556c59c4
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-12-10 11:23:02 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/README.md
@@ -1,10 +1,10 @@
 # [level 1] 로또의 최고 순위와 최저 순위 - 77484 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77484) 
 
 ### 성능 요약
 
-메모리: 72.6 MB, 시간: 0.03 ms
+메모리: 79.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 30일 09:54:21
+2024년 12월 10일 11:22:59
 
 ### 문제 설명
 
--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,6 +1,8 @@
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
+        //맞을 수 있는 숫자 개수
         int possibleNum = 0;
+        //정답 배열
         int[] answer = new int[2];
         
         //0의 개수 구하기
@@ -14,15 +16,17 @@ class Solution {
         int count = 0;
         for(int i=0; i<lottos.length; i++){      
             for(int j=0; j<win_nums.length; j++){
-                if(lottos[i] == 0) break;
+                if(lottos[i] == 0) break; // 이미 위에서 셌어서. . .
                 if(lottos[i] == win_nums[j]){
                     count += 1;
                     break;
                 }
             }    
         }
-        //count 와 possibleNum을 합쳐서 계산
+        
+        //최고 순위
         answer[0] = possibleNum+count == 0 ? 6 : 7-(possibleNum+count);
+        //최저 순위
         answer[1] = count == 0 ? 6 : 7-count;
         
         return answer;
