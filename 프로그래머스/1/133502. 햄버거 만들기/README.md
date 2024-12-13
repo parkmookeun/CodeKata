a62f481616b2afe716e1955508e48eb2bf8c989c
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2024-12-13 17:46:23 +0900

    [level 1] Title: 햄버거 만들기, Time: 120.76 ms, Memory: 153 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/README.md
+++ 프로그래머스/1/133502. 햄버거 만들기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 13일 17:46:00
+2024년 12월 13일 17:46:23
 
 ### 문제 설명
 
--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -2,42 +2,83 @@ import java.util.*;
 class Solution {
      public int solution(int[] ingredient) {
         int answer = 0;
+        int idx = 0;
+        Stack<Integer> candidate = new Stack<>();
+        int[] sequence = new int[]{1,2,3,1};
+        int[] willUsed = new int[4];
+        boolean[] isUsed = new boolean[ingredient.length];
 
-        Stack<Integer> orderList = new Stack<>();
-        int[] popedList = new int[4];
-        int[] sequence = {1,3,2,1};
-        boolean canMake = false;
+//        while(idx < ingredient.length){
+//            //빵이 아니면 검사 x
+//            if(ingredient[idx] != 1){
+//                idx++;
+//                continue;
+//            }
+//            //빵이면 검사 시작
+//            //현재 빵부터 햄버거가 만들어지면
+//            if(candidate.isEmpty() || candidate.peek() != idx){
+//                candidate.push(idx);
+//            }
+//
+//            boolean result = checkHamburger(sequence, ingredient, isUsed,willUsed, candidate.peek());
+//            //결과가 참이면 개수 + 1 && 전에 1인 곳으로 돌아가기
+//            //결과가 거짓이면 그냥 다음으로 넘어가기
+//            if(result){
+//                answer += 1;
+//                candidate.pop();
+//                idx = candidate.isEmpty() ? idx + 4 : candidate.peek();
+//            }else{
+//                idx ++;
+//            }
+//        }
+        int count = 0;
 
         for(int i=0; i<ingredient.length; i++){
-            orderList.push(ingredient[i]);
+            if(ingredient[i] == 1) count++;
 
-            //orderList의 크기가 4이상이면 햄버거를 만들 수 있는지 검사
-            if(orderList.size() >= 4){
-                //햄버거 만들 수 있으면
-                canMake = true;
-                for(int j=0; j<4; j++){
-                    popedList[j] = orderList.pop();
-                }
-                //팝된 요소와 햄버거 순서 비교
-                for(int k=0; k<4; k++){
-                    if(popedList[k] != sequence[k]){
-                        canMake = false;
-                        break;
-                    }
-                }
-                if(canMake){
-                    answer += 1;
-                }else {
-                    //다시 넣어주기
-                    for(int j=0; j<4; j++){
-                        orderList.push(popedList[3-j]);
-                    }
-                }
+        }
+        if(count % 2 == 1){
+            count++;
+        }
+        while(candidate.size() < count / 2){
+            if(ingredient[idx] == 1) candidate.push(idx);
+            idx++;
+        }
 
-                //햄버거 못 만들면
+        while(!candidate.isEmpty()){
+
+            boolean result = checkHamburger(sequence, ingredient, isUsed,willUsed, candidate.peek());
+            //결과가 참이면 개수 + 1 && 전에 1인 곳으로 돌아가기
+            //결과가 거짓이면 그냥 다음으로 넘어가기
+            if(result){
+                answer += 1;
             }
+            candidate.pop();
+
         }
 
         return answer;
+    }
+
+ boolean checkHamburger(int[] sequence, int[] ingredient, boolean[] isUsed, int[] willUsed, int idx){
+        //햄버거가 완성되는지 검사
+        //1->2->3->1 이되는지, 1부터 검사 시작해서 이미 사용된 재료면 건너뛰기
+        //재료를 검사하다가 재료 칸을 벗어나게 되면 중지
+        for(int i=0; i<4; i++){
+            while((idx+i) < ingredient.length && isUsed[idx+i]){
+                idx += 4;
+            }
+
+            if((idx+i) >= ingredient.length || sequence[i] != ingredient[idx+i]){
+
+                return false;
+            }
+            willUsed[i] = idx + i;
+        }
+        for(int i=0; i<4; i++){
+            isUsed[willUsed[i]] = true;
+        }
+
+        return true;
     }
 }
