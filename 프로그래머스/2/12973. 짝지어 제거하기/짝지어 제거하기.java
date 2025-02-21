b6457154de546373de471d5dd71605d7fda85b04
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-02-21 11:35:10 +0900

    [level 2] Title: 짝지어 제거하기, Time: 74.07 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,37 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        
+        Stack stack = new Stack<Character>();
+        
+        int idx = 0;
+        while(idx < s.length()){
+            
+            if(stack.isEmpty()){
+                stack.push(s.charAt(idx));
+                idx++;
+                continue;
+            }    
+            
+            if(stack.peek().equals(s.charAt(idx))){
+                stack.pop();
+            }else{
+                stack.push(s.charAt(idx));
+            }
+            
+            idx++;
+        }
+        
+        if(stack.isEmpty()){
+            answer = 1;
+        }else{
+            answer = 0;
+        }
+        
+        return answer;
+    }
+}
