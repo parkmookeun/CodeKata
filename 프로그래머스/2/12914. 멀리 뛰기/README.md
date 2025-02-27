a64afe04e8bf1d7b497e04717a1665a225ccb450
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-02-27 22:11:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 78.1 MB, 시간: 0.18 ms
+메모리: 81.4 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 25일 11:22:25
+2025년 02월 27일 22:11:50
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -1,15 +1,20 @@
+// 다이나믹 프로그래밍
+// dp(n) = dp(n-2) + dp(n-1)
+
 class Solution {
     public long solution(int n) {
-        long[] dp = new long[n+1];
+        long answer = 0;
+        long[] dp = new long[2001];
         
-        if(n<=2){
-            return n;
-        }
         dp[1] = 1;
         dp[2] = 2;
         
+        if(n <= 2){
+            return n;
+        }
+        
         for(int i=3; i<=n; i++){
-            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+            dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
         }
         
         return dp[n];
