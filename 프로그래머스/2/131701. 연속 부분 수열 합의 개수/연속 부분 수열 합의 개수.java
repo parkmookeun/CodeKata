73cdfc343bd13f50826b993c17d3aed5980c8752
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-04 13:36:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 368.25 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,28 +1,39 @@
-import java.util.*;
+//핵심 아이디어: 원형 수열 표현을 어떻게 할 건지 & 중복 값 제외 (Set)
+// -> 원형은 배열의 마지막 인덱스 다음 인덱스를 처음으로 다시 돌려주는 로직으로 표현
+import java.util.HashSet;
+import java.util.Set;
 class Solution {
     public int solution(int[] elements) {
         int answer = 0;
-        //[][] 각각 길이와 시작 인덱스
-        int[][] subTotal = new int[elements.length+1][elements.length];
-        Set<Integer> set = new HashSet<>();
-        //초기화
+        int start = 0;
+        int temp = 0;
+        
+        Set<Integer> sets = new HashSet<>();
+        
         for(int i=0; i<elements.length; i++){
-            subTotal[1][i] = elements[i];
-            set.add(subTotal[1][i]);
-//            System.out.println(subTotal[1][i]);
-        }
-        System.out.println("구분");
-        //배열에 값 넣기
-        for(int i=2; i<=elements.length; i++){
-//            System.out.println("길이: " + i);
-            for(int j=0; j<elements.length; j++){
-                subTotal[i][j] = subTotal[i-1][j]+elements[(j+i-1)%elements.length];
-//                System.out.println(subTotal[i][j]);
-                set.add(subTotal[i][j]);
+            //시작점
+            start = i;
+            temp = elements[i];
+            sets.add(elements[i]);
+            
+            start += 1;
+            if(start >= elements.length){
+                    start = 0;
+            }
+            //시작점으로 다시 돌아올때까지 가능한 수 sets에 추가
+            while(start != i){
+                temp += elements[start];
+                sets.add(temp);
+                start++;
+                
+                if(start >= elements.length){
+                    start = 0;
+                }
             }
         }
-
-        answer = set.size();
+        
+        answer = sets.size();
+        
         return answer;
     }
 }
