3d35cc21d51c1a69e857f838da828b922a29af1d
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-04 11:41:55 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,16 +1,24 @@
+//핵심 아이디어: 다음 라운드에 진출하게 되면 다시 1번 부터 N/2까지 다시 배정
+//홀수는 항상 + 1인 짝수와 대결을 한다.
+//홀수와 짝수가 있을 때, 그 둘이 이기면 갖는 숫자가 동일할때 대결! 
+
 class Solution
 {
     public int solution(int n, int a, int b)
     {
-        
         int answer = 0;
-        while(true){
-            a = a/2 + a%2;
-            b = b/2 + b%2;
+        
+        
+        while(a != b){
+            a = a % 2 == 1 ? a+1 : a;
+            b = b % 2 == 1 ? b+1 : b;
+            
+            a /= 2;
+            b /= 2;
             
             answer++;
-            if(a == b) break;
         }
+
         return answer;
     }
 }
