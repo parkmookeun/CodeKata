fcd4cf8737b943ebe735201d2dfd92876dcd0a8f
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-05 16:33:13 +0900

    [level 2] Title: 괄호 회전하기, Time: 196.66 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 95.9 MB, 시간: 51.47 ms
+메모리: 110 MB, 시간: 196.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 09일 09:56:44
+2025년 03월 05일 16:33:12
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,45 +1,55 @@
-import java.util.*;
+//핵심 아이디어 : 괄호의 회전 표현방식(자료구조) + 올바른 괄호인지 검사 로직
+// 큐 + 스택
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 class Solution {
     public int solution(String s) {
+        int length = 0;
         int answer = 0;
-        List<String> list = new ArrayList<>(Arrays.asList(s.split("")));
-
+        Deque<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> map = new HashMap<>();
+        
+        map.put('{','}');
+        map.put('[',']');
+        map.put('(',')');
+        
+        // s의 문자열을 전부 queue에 넣는다.
         for(int i=0; i<s.length(); i++){
-            answer += checkList(list);
-            String remove = list.remove(0);
-            list.add(remove);
+            queue.add(s.charAt(i));
         }
-        return answer;
-    }
+        
+        // queue의 문자열을 스택에 적용하여 올바른 괄호인지 검사한다.
+        for(int i=0; i<queue.size(); i++){
+            stack.clear();
+            length = 0;
+        
+            while(length < s.length()){
 
-    private int checkList(List<String> list) {
-        Stack<String> stack = new Stack<>();
-        Map<String,String> map = new HashMap<>();
-        map.put("(",")");
-        map.put("{","}");
-        map.put("[","]");
-        
-        
-        int idx = 0;
-        while(idx < list.size()){
-            String value = list.get(idx);
-            if(value.equals("{") || value.equals("[") || value.equals("(")){
-                stack.push(value);
-            }else{
-                if (stack.isEmpty()){
-                    return 0;
+                Character qChar = queue.pollFirst();
+
+                if(stack.isEmpty() || map.get(stack.peek()) != qChar){
+                    stack.push(qChar);
                 }
-                String peek = stack.peek();
-                if(!map.get(peek).equals(value)){
-                    return 0;
+
+                if(map.get(stack.peek()) == qChar){
+                    stack.pop();
                 }
-                stack.pop();
+
+                queue.add(qChar);
+                length++;
             }
-            idx++;
-        }
-        if(!stack.isEmpty()){
-            return 0;
-        }
-        return 1;
+            if(stack.isEmpty()){
+                answer++;
+            }
+            // 길이만큼의 검사가 끝나면, 첫번째 값을 빼서 마지막 갚에 넣어준다.
+            queue.add(queue.pollFirst());
+            }
+            return answer;
     }
 }
