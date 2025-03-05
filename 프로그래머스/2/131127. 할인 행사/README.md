be13b450bd7c72d8e9aa96f6697ba682fd6960d4
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-05 17:52:52 +0900

    [level 2] Title: 할인 행사, Time: 67.05 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127#qna) 
 
 ### 성능 요약
 
-메모리: 130 MB, 시간: 49.09 ms
+메모리: 105 MB, 시간: 67.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 12일 16:22:46
+2025년 03월 05일 17:52:51
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,54 +1,66 @@
-import java.util.*;
+// 핵심 아이디어 : 소비자가 구매를 원하는 모든 항목을 할인 가격으로 살수 있는 날에 회원가입
+// 당사자의 구매 희망물품과 그 개수를 맵에 저장
+// 1일부터 10일간의 할인물품과 그 갯수 맵에 저장
+// discount의 요소를 1-10일까지 큐에 저장 -> 하루하루 지나면서, 앞 품목 제거 및 담날 품목 추가
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Deque;
+    
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        //내가 원하는 품목의 종류와 개수를 담을 맵
-        Map<String,Integer> wantedMap = new HashMap<>();
-        //discount 배열에서 처음 10일간의 할인 품목의 종류와 개수를 담을 맵
-        Map<String,Integer> discountMap = new HashMap<>();
-        //discount 배열의 품목들을 계속해서 넣고 뺄 큐
-        Queue<String> discountQue = new LinkedList<>();
-
-        //원하는 품목 맵 초기화
+        
+        Map<String,Integer> toBuy = new HashMap<>();
+        Map<String,Integer> toDiscount = new HashMap<>();
+        Deque<String> disCount = new LinkedList<>();
+        
+        // 내 희망 목록과 그 갯수 맵에 저장
         for(int i=0; i<want.length; i++){
-            wantedMap.put(want[i],number[i]);
+            toBuy.put(want[i],number[i]);        
         }
-
-
-        //처음 10일간 할인 품목 맵 초기화 및 큐 초기화
+        // 1-10일까지의 할인 품목 목록과 그 갯수 맵에 저장
         for(int i=0; i<10; i++){
-            Integer value = discountMap.get(discount[i]) == null ? 1 : discountMap.get(discount[i])+1;
-            discountMap.put(discount[i],value);
-            discountQue.add(discount[i]);
+            toDiscount.put(discount[i], toDiscount.getOrDefault(discount[i],0)+1);    
         }
+        // 1-10일까지 할인 품목 목록 큐에 저장
+        for(int i=0; i<10; i++){
+            disCount.add(discount[i]);
+        }
+        
+        
+        // discount 배열 길이만큼 계속해서 회원가입 할 날짜 찾기
+        int startIdx = 9;
+        boolean isOk = false;
+        
+        while(startIdx <= discount.length-1){
+            isOk = true;
+            //모두 할인 가능한지 검사
+            for(int i=0; i<want.length; i++){
+                if(toBuy.get(want[i]) > toDiscount.getOrDefault(want[i],0)){
+                    isOk = false;
+                }    
+            }
+            
+            if(isOk) answer++;
 
-
-        for(int i=10; i<=discount.length; i++){
-            //비교
-            answer += compareList(wantedMap,discountMap);
-            if(i == discount.length){
+            
+            startIdx++;
+            
+            if(startIdx == discount.length){
                 break;
             }
-            //큐에서 넣고 빼기
-            String removed = discountQue.remove();
-            discountQue.add(discount[i]);
-            //discount맵도 반영
-            discountMap.put(removed,discountMap.get(removed)-1);
-            Integer value = discountMap.get(discount[i]) == null ? 1 : discountMap.get(discount[i])+1;
-            discountMap.put(discount[i],value);
-
+            
+            //큐에서 삭제하고 새값 추가
+            String removed = disCount.pollFirst();
+            disCount.add(discount[startIdx]);
+            
+            //큐에서 삭제한 값 및 새값 -> 맵에 반영
+            toDiscount.put(removed, toDiscount.getOrDefault(removed,0) -1);
+            toDiscount.put(discount[startIdx], toDiscount.getOrDefault(discount[startIdx],0)+1);
 
         }
+        
         return answer;
     }
-
-    private int compareList(Map<String, Integer> wantedMap, Map<String, Integer> discountMap) {
-        for (String s : wantedMap.keySet()) {
-            if(wantedMap.get(s) > discountMap.getOrDefault(s,0)){
-                return 0;
-            }
-        }
-        return 1;
-    }
-
 }
