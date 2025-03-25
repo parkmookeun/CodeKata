5d9b3cf06ab1b6f4bee16166294faf8faf6805f2
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-25 19:41:12 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,16 +2,20 @@
 // 각 경우마다 검사해서 brown 개수가 되면 답이니까 break
 class Solution {
     public int[] solution(int brown, int yellow) {
+        //정답 배열
         int[] answer = new int[2];
         
+        // 가로, 세로
         int row = 0;
         int col = 0;
         
+        // 가로 길이 >= 세로 길이 이므로
         for(int i=1; i<= (int)(Math.sqrt(yellow)); i++){
             if(yellow % i == 0){
                 col = i;
                 row = yellow / col;        
                 
+                // yellow를 덮는 타일이 brown일때
                 if(((row+2)*2 + col*2) == brown){
                     break;
                 }
@@ -19,7 +23,7 @@ class Solution {
         }
         
         
-        
+        // 정답 추출
         answer[0] = row+2;
         answer[1] = col+2;
         
