d46bfe78a1657021fd9decf4c1017bf60048dec2
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-03-26 12:27:06 +0900

    [level 1] Title: 체육복, Time: 0.54 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,44 +1,43 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
-        //학생들 체육복 수 체크
-        int[] students = new int[n+1];
-        Arrays.fill(students,1);
-        //lost 체크
+        //진짜 빌려줄 수 있는 친구들
+        int[] friends = new int[n+1];
+        
+        Arrays.fill(friends,1);
+        
+        // server 초기화
         for(int i=0; i<lost.length; i++){
-            students[lost[i]] -= 1;
+            friends[lost[i]] -= 1;
         }
-
-        //reserve 체크
+        
         for(int i=0; i<reserve.length; i++){
-            students[reserve[i]] += 1;
+            friends[reserve[i]] += 1;
         }
-
-        //이제 줄수 있는지 체크
+        
+        //초기화 후에 현재 체육복 있는 친구들 검사
+        int answer = 0;
+        for(int i=1; i<friends.length; i++){
+            if(friends[i] >= 1) answer++;
+        }
+        //lost를 돌면서 왼쪽부터 빌릴 수 있는지 검사 후, 없으면 오른쪽에서 빌릴 수 있는지 검사
+        //기준으로 왼쪽 다음 오른쪽으로 정했기 때문에 lost배열 정렬
         Arrays.sort(lost);
+        
         for(int i=0; i<lost.length; i++){
-            //이미 있으면 넘어가기
-            if(students[lost[i]] > 0){
-                continue;
-            }
-            //왼쪽에서 받을 수 있으면 받기
-            if(lost[i]-1 > 0 && students[lost[i]-1] > 1){
-                students[lost[i]] += 1;
-                students[lost[i]-1] -= 1;
-                continue;
-            }
-            //오른쪽에서 받을 수 있으면 받기
-            if(lost[i]+1 <= n && students[lost[i]+1] > 1){
-                students[lost[i]] += 1;
-                students[lost[i]+1] -= 1;
+            
+            if(friends[lost[i]] > 0) continue;
+            
+            if((lost[i]-1) > 0 && friends[lost[i]-1] > 1){
+                friends[lost[i]-1] -= 1;
+                answer++;
+            }else if((lost[i]+1) <= n && friends[lost[i]+1] > 1){
+                friends[lost[i]+1] -= 1;
+                answer++;
             }
         }
-        //이제 체육복을 가진 친구들 세기
-        for(int i=1; i<=n; i++){
-            if(students[i] >= 1) answer += 1;
-        }
+        
         return answer;
     }
 }
