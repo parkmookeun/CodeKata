92b6b0ac8f27f3810a5631c9066edf9326c2efe3
parkmookeun <94534687+parkmookeun@users.noreply.github.com>
2025-07-09 09:02:09 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 77.63 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 프렌즈4블록 - 17679 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 81.67 ms
+메모리: 144 MB, 시간: 77.63 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 23:17:22
+2025년 07월 09일 09:02:07
 
 ### 문제 설명
 
--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -24,40 +24,34 @@ class Solution {
             }
         }
         
+        //while문을 계속 돌릴지 여부를 결정하는 변수
         boolean isSquareExisted = true;
-        // System.out.println("=== 오리지날 ===");
-        // printDeepArrays(boardMap);
-        // System.out.println("==========");
+        
+        
         while(isSquareExisted){
             Set<Point> tileSet = new HashSet<>();
             
             //타일 검사 쫙 하기
             for(int i=0; i<m; i++){
                 for(int j=0; j<n; j++){
+                    //사라질 위치면 disAppear에 담김
                    int[][] disAppear = findSquare(boardMap, m, n, i, j);
-                    // System.out.println(Arrays.deepToString(disAppear));
                    for(int k=0; k<4; k++){
-                   if(disAppear[k][0] == -1) break;
-                   tileSet.add(new Point(disAppear[k][0],disAppear[k][1]));
+                   if(disAppear[k][0] == -1) break; // 아무것도 없으면 끝
+                   tileSet.add(new Point(disAppear[k][0],disAppear[k][1])); // tileSet에 위치 담기
                    }
                 }
             }
-            
-            // System.out.println(tileSet);
-            
+                        
             //Set에 들어간 개수 -> answer 에 더해주기
-            // System.out.println("없어진 타일 개수: " + tileSet.size());
             answer += tileSet.size();
             
             //Set에 들어간 타일 빈칸으로 만들어 주기
             blankSquare(tileSet, boardMap);
-            // System.out.println("없어진 타일 빈칸 채우기");
-            // printDeepArrays(boardMap);
+            
             //이제 위치 변경해주기
             moveBoard(boardMap);
-            // System.out.println("타일 옮기기");
-            // printDeepArrays(boardMap);
-            // System.out.println("==================");
+            
             if(tileSet.isEmpty()) isSquareExisted = false;
         }
         
@@ -73,17 +67,15 @@ class Solution {
         squareTiles[tileIdx][0] = x;
         squareTiles[tileIdx][1] = y;
         
-        
+        //무슨 블록인지 저장
         char value = board[x][y];
-        //
+        
+        //만약 빈칸이라면 빈 배열 리턴
         if(value == '0'){
-         int[][] nothingArr = new int[4][2];
-            for(int[] arr : nothingArr){
-                Arrays.fill(arr, -1);
-            }
-            return nothingArr;
+            return generateBlankArray();
         }
         
+        // 나머지 3칸 검사(아래,대각선,오른쪽)
         int[] dx = {1,1,0};
         int[] dy = {0,1,1};
         
@@ -91,20 +83,14 @@ class Solution {
             int sx = x + dx[i];
             int sy = y + dy[i];
             
+            //가장자리 검사
             if(sx >= m || sy >= n){
-                int[][] nothingArr = new int[4][2];
-                for(int[] arr : nothingArr){
-                    Arrays.fill(arr, -1);
-                }
-                return nothingArr;
+                return generateBlankArray();
             } 
             
+            //블록 값이 다르면
             if(board[sx][sy] != value){
-                int[][] nothingArr = new int[4][2];
-                for(int[] arr : nothingArr){
-                    Arrays.fill(arr, -1);
-                }
-                return nothingArr;
+                return generateBlankArray();
             }
             
             //직사각형인 경우
@@ -116,6 +102,7 @@ class Solution {
         return squareTiles;
     }
     
+    //보드 빈칸 처리
     public void blankSquare(Set<Point> set, char[][] board){
         for(Point tile : set){
             board[tile.x][tile.y] = '0';
@@ -130,6 +117,7 @@ class Solution {
             int zeroCount = 0;
             StringBuilder sb = new StringBuilder();
             
+            // 0 개수를 세고, 나머지는 sb에 저장
             for(int j=0; j<board.length; j++){
                 if(board[j][i] == '0') zeroCount++;
                 else{
@@ -137,11 +125,13 @@ class Solution {
                 }
             }
             
+            // sb 앞에 0을 개수대로 추가
             sb.insert(0,"0".repeat(zeroCount));
             
-            if(zeroCount != 0)
+            if(zeroCount != 0){
             for(int j=0; j<board.length; j++){
                 board[j][i] = sb.toString().charAt(j);
+                }
             }
         }
     }
@@ -150,6 +140,14 @@ class Solution {
         for(char[] arr : board){
             System.out.println(Arrays.toString(arr));
         }
+    }
+    
+    public int[][] generateBlankArray(){
+        int[][] nothingArr = new int[4][2];
+            for(int[] arr : nothingArr){
+                Arrays.fill(arr, -1);
+            }
+            return nothingArr;
     }
     
     class Point{
